3232cb7dcaf0114d2f1c4d77017571cbd9c43b39
Marte0 <matteodellagostino@gmail.com>
2024-03-11 14:05:40 +0100

    miao
    j

---
+++ client_server_chatgpt4_traduttore_asia/client.js
@@ -0,0 +1,224 @@
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js";
+
+//start experience variabili
+const intervalTime = 30000;
+let isRunning = false;
+const startButton = document.getElementById("bottone");
+
+const supabaseUrl = "YOUR SUPABASE URL";
+const supabaseKey = "YOUR SUPABASE KEY";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+//variabili canvas
+const canvas = document.getElementById("myCanvas");
+const context = canvas.getContext("2d", { willReadFrequently: true });
+
+//variabili audio
+const audioKey = "YOUR ELEVENLABS KEY";
+
+//variabili immagini
+let imageWidth = canvas.width;
+let imageHeight;
+let image1 = new Image();
+image1.crossOrigin = "Anonymous";
+let image2 = new Image();
+image2.crossOrigin = "Anonymous";
+
+image1.onload = () => {
+  console.log("image 1 ha loddato");
+  imageHeight = canvas.width / (image1.width / image1.height);
+  context.drawImage(image1, 0, 0, imageWidth, imageHeight);
+  // currentLoaded = true;
+};
+image1.src = "https://cbkfspvaaiewasutjomn.supabase.co/storage/v1/object/public/news-to-image/1709886951057.jpg";
+
+startButton.addEventListener("click", () => {
+  startButton.style.display = "none";
+  setInterval(() => {
+    if (!isRunning) {
+      isRunning = true;
+      console.log("---------------------FACCIO LE COSE---------------------");
+      fetchLastUploadedImage().then((obj) => {
+        if (!obj.used) {
+          console.log("Last uploaded image:", obj);
+          image2.src = obj.href;
+          image2.onload = () => {
+            emptyCartillo();
+            textToSpeech(obj.description);
+            pixelTransition(image1, image2).then(() => {
+              image1 = image2;
+              image2 = new Image();
+              image2.crossOrigin = "Anonymous";
+              console.log("ho sostituito le immagini");
+              editCartillo(obj);
+              updateState(obj);
+            });
+          };
+        } else {
+          console.log("ho gia usato questa immagine, testa di cazzo");
+          isRunning = false;
+        }
+      });
+    }
+  }, intervalTime);
+});
+// Example usage
+
+async function updateState(obj) {
+  isRunning = false;
+  console.log("uploadando stato: " + isRunning);
+  const { data, error } = await supabase.from("asia").upsert({ id: obj.id, used: "TRUE" }).select();
+}
+
+async function fetchLastUploadedImage() {
+  const { data, error } = await supabase
+    .from("asia") // Replace with your table name
+    .select("*") // Assuming your column storing the image URL is named 'image_url'
+    .order("id", { ascending: false }) // Replace 'created_at' with your timestamp or ID column
+    .limit(1)
+    .single(); // Ensures that the response structure is simplified for single row
+
+  if (error) {
+    console.error("Error fetching the last uploaded image URL:", error);
+    return null;
+  }
+
+  return data; // Return the URL of the last uploaded image
+}
+
+function editCartillo(obj) {
+  document.getElementById("passedTime").innerHTML = calculatePassedTime(obj) + " minutes ago";
+  document.getElementById("currentDateTime").innerHTML = obj.date_time;
+  document.getElementById("timeZone").innerHTML = "(UTC" + obj.time_zone + ")";
+  if (obj.state == "") {
+    document.getElementById("continent").style.fontSize = "2.4375rem";
+    document.getElementById("state").display = "none";
+  } else {
+    document.getElementById("continent").style.fontSize = "1.8125rem";
+    document.getElementById("state").display = "block";
+  }
+  document.getElementById("continent").innerHTML = "asia";
+  document.getElementById("from").innerHTML = "From";
+  document.getElementById("state").innerHTML = obj.state;
+  document.getElementById("prompt").innerHTML = obj.description;
+}
+
+function emptyCartillo() {
+  document.getElementById("passedTime").innerHTML = "";
+  document.getElementById("currentDateTime").innerHTML = "";
+  document.getElementById("timeZone").innerHTML = "";
+  document.getElementById("continent").innerHTML = "Loading...";
+  document.getElementById("state").innerHTML = "";
+  document.getElementById("prompt").innerHTML = "";
+  document.getElementById("from").innerHTML = "";
+}
+
+function calculatePassedTime(obj) {
+  let passedTime = Date.now() - obj.id;
+  passedTime = Math.floor(passedTime / 60000);
+  return passedTime;
+}
+
+function pixelate(divisionNumber, image, alpha = 1) {
+  context.globalAlpha = alpha;
+  let chunk = imageWidth / divisionNumber;
+  context.drawImage(image, 0, 0, imageWidth, imageHeight);
+  context.globalAlpha = 1;
+  let imageData = context.getImageData(0, 0, canvas.width, canvas.height).data;
+
+  for (let y = chunk / 2; y <= canvas.height; y += chunk) {
+    for (let x = chunk / 2; x <= canvas.width; x += chunk) {
+      const pixelIndexPosition = (Math.floor(x) + Math.floor(y) * canvas.width) * 4;
+
+      context.fillStyle = `rgba(${imageData[pixelIndexPosition]},${imageData[pixelIndexPosition + 1]},${imageData[pixelIndexPosition + 2]},${imageData[pixelIndexPosition + 3] * alpha})`;
+
+      context.fillRect(x - chunk / 2, y - chunk / 2, chunk + 1, chunk + 1);
+    }
+  }
+
+  return true;
+}
+
+function pixelTransition(currentImage, nextImage, maxDivisions = 100, minDivisions = 10, step = 10, intervalTime = 200) {
+  return new Promise((resolve) => {
+    const stepsN = (maxDivisions - minDivisions) / step;
+    //const intervalTime = 200;
+    const alphaStep = 1 / (stepsN * 2);
+    let currentDivisions = maxDivisions;
+    let alpha = alphaStep;
+    let counter = 0;
+
+    console.log("---------STEPSN = " + stepsN);
+
+    let pixelateInterval = setInterval(() => {
+      counter++;
+      if (counter >= stepsN) {
+        clearInterval(pixelateInterval);
+        counter = 0;
+        let depixelateInterval = setInterval(() => {
+          counter++;
+          if (counter >= stepsN) {
+            clearInterval(depixelateInterval);
+            setTimeout(() => {
+              context.drawImage(nextImage, 0, 0, imageWidth, imageHeight);
+              resolve();
+            }, intervalTime);
+          }
+          pixelate(currentDivisions, currentImage);
+          pixelate(currentDivisions, nextImage, alpha);
+          alpha += alphaStep;
+          currentDivisions += step;
+        }, intervalTime);
+      }
+      pixelate(currentDivisions, currentImage);
+      pixelate(currentDivisions, nextImage, alpha);
+      alpha += alphaStep;
+      currentDivisions -= step;
+    }, intervalTime);
+  });
+}
+
+//Generate audio
+async function textToSpeech(instruction) {
+  //const text = instruction; // Ensure this variable contains the text to convert
+  const voiceId = "21m00Tcm4TlvDq8ikWAM";
+
+  const headers = new Headers();
+  headers.append("Accept", "audio/mpeg");
+  headers.append("xi-api-key", audioKey);
+  headers.append("Content-Type", "application/json");
+
+  const body = JSON.stringify({
+    text: instruction, // Make sure this matches the expected format
+    model_id: "eleven_multilingual_v1",
+    voice_settings: {
+      stability: 0.99,
+      similarity_boost: 0.1,
+      style: 0.15,
+      speed: 0.5,
+    },
+  });
+
+  try {
+    // Make sure the URL is correct and matches the API documentation
+    const response = await fetch(`https://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream`, {
+      method: "POST",
+      headers: headers,
+      body: body,
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text(); // Improved error feedback
+      console.error(`Error: ${response.status} - ${response.statusText}`);
+      console.error("Response Text:", errorText); // Displaying the detailed error message
+      throw new Error(`Text to Speech API request failed with status ${response.status}`);
+    }
+
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+    const audio = new Audio(url);
+    audio.play();
+  } catch (error) {
+    console.error("Error in ElevenLabs TTS API request:", error);
+  }
+}
---
+++ client_server_chatgpt4_traduttore_europe/server.js
@@ -0,0 +1,268 @@
+// Import the required modules
+const { createClient } = require("@supabase/supabase-js");
+const fetch = require("node-fetch");
+const OpenAI = require("openai");
+const Rettiwt = require("rettiwt-api").Rettiwt;
+
+const archivio = require("./europe.json");
+
+// Prompt generation from twitter
+const twitterKey = "YOUR TWITTER API KEY";
+const rettiwt = new Rettiwt({ apiKey: twitterKey });
+let lastTweet = "";
+let account = "";
+
+// Image upload
+const bucketName = "news-to-image";
+const supabaseUrl = "YOUR SUPABASE URL";
+const supabaseKey = "YOUR SUPABASE API KEY";
+const supabase = createClient(supabaseUrl, supabaseKey);
+let imageUrl = "";
+
+//Prompt translation
+const translatorKey = "YOUR MICROSOFT TRANSLATOR KEY";
+const endpoint = "https://api.cognitive.microsofttranslator.com/";
+
+async function promptTranslation(text) {
+  const url = `${endpoint}/translate?api-version=3.0&to=en`;
+  const options = {
+    method: "POST",
+    headers: {
+      "Ocp-Apim-Subscription-Key": translatorKey,
+      "Ocp-Apim-Subscription-Region": "northeurope", // Specifica la regione del tuo servizio, es. "westeurope"
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify([{ Text: text }]),
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const jsonResponse = await response.json();
+    const instruction = jsonResponse[0].translations[0].text;
+    promptGeneration(instruction);
+    console.log("Lingua rilevata:", jsonResponse[0].detectedLanguage.language);
+    console.log("Testo tradotto:", jsonResponse[0].translations[0].text);
+  } catch (error) {
+    console.error("Errore nella traduzione:", error);
+  }
+}
+
+// Image generation
+const openai = new OpenAI({
+  organization: "YOUR OPENAI ORGANIZATION",
+  apiKey: "YOUR OPENAI API KEY",
+});
+
+async function promptGeneration(instruction) {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: `Rewrite the following text, making it a perfect prompt for DALL-E 3 to create a vertical oriented image, adding all the details so that the scene described by the text is as well-described as possible. Reach a maximum of 2000 characters without ever creating a new paragraph. Adapt the final prompt so as not to violate your policy. Do not add quotes within the text. Specify that the image should be a photo, avoiding collages and illustrations: ${instruction}`,
+      },
+    ],
+    model: "gpt-4-turbo-preview",
+    temperature: 0,
+    top_p: 0,
+  });
+  console.log(completion.choices[0].message.content);
+  const description = instruction;
+  instruction = completion.choices[0].message.content;
+  image_generate(instruction, description);
+}
+
+const image_generate = async (instruction, description, retry = 0) => {
+  try {
+    const response = await openai.images.generate({
+      model: "dall-e-3",
+      prompt: instruction,
+      n: 1,
+      size: "1024x1792",
+      quality: "hd",
+      style: "natural",
+    });
+    console.log("response", response.data[0].url);
+    imageUrl = response.data[0].url;
+    const id_image = new Date().getTime();
+    const pathInBucket = id_image + ".webp";
+    console.log(id_image + " , " + imageUrl + " , " + bucketName + " , " + pathInBucket + " , " + description);
+    uploadImageToSupabase(id_image, imageUrl, bucketName, pathInBucket, description);
+  } catch (error) {
+    console.log("BUUU ERRORE");
+    if (retry > 1) {
+      getTweet();
+    } else {
+      image_generate(instruction, description, retry++);
+      console.log(retry);
+    }
+  }
+};
+
+// Function to upload an image to Supabase Storage
+async function uploadImageToSupabase(id_image, imageUrl, bucketName, pathInBucket, description) {
+  try {
+    // Fetch the image from the URL
+    const response = await fetch(imageUrl);
+    if (!response.ok) throw new Error("Image fetch failed");
+
+    // Get the image buffer
+    const buffer = await response.buffer();
+
+    // Upload the image to Supabase Storage
+    const { data, error } = await supabase.storage.from(bucketName).upload(pathInBucket, buffer, {
+      contentType: response.headers.get("content-type"),
+      upsert: true, // Set to true to overwrite an existing file with the same path
+    });
+
+    if (error) {
+      setTimeout(() => {
+        uploadImageToSupabase(id_image, imageUrl, bucketName, pathInBucket, description);
+      }, 30000);
+      throw error;
+    }
+
+    console.log("Upload successful");
+    insertImageRecord(id_image, `https://cbkfspvaaiewasutjomn.supabase.co/storage/v1/object/public/news-to-image/${pathInBucket}`, description, account.state, account.timezone, formatDateAndTimeWithOffset(account.timezone - 1));
+
+    return data;
+  } catch (error) {
+    console.log("BUUU ERRORE");
+    if (retry > 1) {
+      getTweet();
+    } else {
+      retry++;
+      console.log("retry: " + retry);
+      image_generate(instruction, description, retry);
+    }
+  }
+}
+
+// create table row----------------------------------------------------
+async function insertImageRecord(id_image, imagePath, description, state, timeZone, date_time) {
+  const { data, error } = await supabase.from("europe").insert([{ id: id_image, href: imagePath, description: description, state: state, time_zone: timeZone, date_time: date_time }]);
+
+  if (error) throw error;
+  start();
+  //return data;
+}
+
+//calculate time and date
+function formatDateAndTimeWithOffset(offsetString) {
+  // Parse the offset string to get the number of hours
+  const offsetHours = parseInt(offsetString, 10);
+
+  // Get the current date and time
+  let currentDate = new Date();
+
+  // Calculate the time offset in milliseconds (offsetHours * number of milliseconds in an hour)
+  const offsetMilliseconds = offsetHours * 60 * 60 * 1000;
+
+  // Adjust the date by the offset
+  currentDate = new Date(currentDate.getTime() + offsetMilliseconds);
+
+  // Format the date and time components
+  const day = currentDate.getDate().toString().padStart(2, "0");
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, "0"); // JavaScript months are 0-indexed
+  const year = currentDate.getFullYear();
+  const hours = currentDate.getHours().toString().padStart(2, "0");
+  const minutes = currentDate.getMinutes().toString().padStart(2, "0");
+  const seconds = currentDate.getSeconds().toString().padStart(2, "0");
+
+  // Combine the components into the final string
+  const formattedDateTime = `${day}.${month}.${year} - ${hours}:${minutes}:${seconds}`;
+
+  return formattedDateTime;
+}
+
+//get tweet
+function getTweet() {
+  rettiwt.tweet
+    .list("1764745922448077015")
+    .then((res) => {
+      if (lastTweet != res.list[0].id) {
+        account = res.list[0].tweetBy.userName;
+        archivio.accounts.forEach((acc) => {
+          if (account == acc.id) {
+            account = acc;
+          }
+        });
+        const instruction = removeUrl(res.list[0].fullText);
+        console.log(instruction);
+        if (instruction.length < 275) {
+          promptTranslation(instruction);
+          lastTweet = res.list[0].id;
+        } else {
+          getTweet();
+        }
+      } else {
+        getTweet();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function removeUrl(str) {
+  const regex = /http\S*\s?|[#@]/g;
+  return str.replace(regex, "");
+}
+
+//getTweet();
+
+//start the server
+async function start() {
+  console.log("--------inizio il processo--------");
+  const { data, error } = await supabase
+    .from("europe") // Replace with your table name
+    .select("*") // Assuming your column storing the image URL is named 'image_url'
+    .order("id", { ascending: false }) // Replace 'created_at' with your timestamp or ID column
+    .limit(1)
+    .single(); // Ensures that the response structure is simplified for single row
+
+  if (error) {
+    console.error("Error fetching the last uploaded image URL:", error);
+    return null;
+  }
+
+  if (data.used) {
+    getTweet();
+  } else {
+    setTimeout(() => {
+      start();
+    }, 1000);
+  }
+}
+
+start();
+
+// Function to upload an image to Supabase Storage
+// async function uploadImageToSupabase(id_image, imageUrl, bucketName, pathInBucket, description) {
+//   try {
+//     // Fetch the image from the URL
+//     const response = await fetch(imageUrl);
+//     if (!response.ok) throw new Error("Image fetch failed");
+
+//     // Get the image buffer
+//     const buffer = await response.buffer();
+
+//     // Upload the image to Supabase Storage
+//     const { data, error } = await supabase.storage.from(bucketName).upload(pathInBucket, buffer, {
+//       contentType: response.headers.get("content-type"),
+//       upsert: true, // Set to true to overwrite an existing file with the same path
+//     });
+
+//     if (error) throw error;
+
+//     console.log("Upload successful");
+//     // insertImageRecord(id_image, `https://cbkfspvaaiewasutjomn.supabase.co/storage/v1/object/public/news-to-image/${pathInBucket}`, description, account.state, account.timezone, formatDateAndTimeWithOffset(account.timezone - 1));
+
+//     return data;
+//   } catch (error) {
+//     console.error("Failed to upload image:", error.message);
+//   }
+// }
+
+// let url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-awOJkPr1OOqOOPrAo2CSyDC2/user-HR83uB4E76U41I5Kqtk2FFWB/img-bd4qbnvpuMdzeOuqfm7waiYb.png?st=2024-03-08T09%3A02%3A57Z&se=2024-03-08T11%3A02%3A57Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-03-07T18%3A07%3A25Z&ske=2024-03-08T18%3A07%3A25Z&sks=b&skv=2021-08-06&sig=%2Bw6lx0OdQkohdWjH2i1N0YAYdeYfMcC73ePCdgObwCo%3D"
+
+// uploadImageToSupabase(1709807065050, url, "news-to-image", "1709807065050.webp", "Piers Morgan sexism row after controversial International Women's Days jibes")
